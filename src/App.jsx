2131e59fe2808377353befc078abfeb587d391ae
Fran Espino <fraporitmos>
2024-10-01 21:46:31 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import Card from './components/Card/Card'
-import Search from './components/Search/Search'
-import db from './database/db'
+import React from 'react'
+
+import Home from './page/Home'
 
 const App = () => {
-
-  const [characters, setCharacters] = useState([])
-
-  useEffect(() => {
-    setCharacters(db)
-  }, [])
-
   return (
     <>
-      <Search />
-      <div style={{
-        width: '100%',
-        display: 'flex',
-        gap: '2em',
-        flexWrap: 'wrap',
-        justifyContent: 'center',
-        marginTop: "2em"
-      }}>
-
-        {
-          characters.length > 0
-            ?
-            characters.map((item, index) => (
-              <Card
-                key={index}
-                name={item.name}
-                health={item.health} 
-                attack={item.attack}
-                kiRestoreSpeed={item.kiRestoreSpeed}
-                defense={item.defense}
-                gender={item.gender}
-                img={item.img}
-                />
-            )
-            )
-            : <h1>Cargando ...</h1>
-        }
-      </div>
+      <Home />
     </>
   )
-
-
 }
 
 export default App
